0a22f9af9492dddbfa0ed8e6c8092e63d8570110
Nick The Sick <nperez0111@gmail.com>
2016-06-01 19:52:48 -0400

    Implementation of search feature

--- app/scripts/views/Main.js
+++ app/scripts/views/Main.js
@@ -2,7 +2,27 @@ var Base = require( './../ractives/Base.js' );
 
 module.exports = Base.extend( {
     template: require( './Main.ract' ).template,
-    onrender: function () {
-        $( '#test' ).modal( "show" );
+    oninit: function () {
+        /*this.observe( "query", ( newV, oldV ) => {
+            if ( !newV ) {
+                return;
+            }
+
+            this.getImage( newV ).then( this.logger, this.logger ).then( cur => {
+                this.set( "display", cur );
+            } );
+        } );*/
+        this.on( "search", ( ev ) => {
+            this.notify( "It Works", this.get( 'query' ) );
+            /*this.getImage( this.get('query') ).then( this.logger, this.logger ).then( cur => {
+                this.set( "display", cur );
+            } );*/
+        } );
+    },
+    data: function () {
+        return {
+            query: "",
+            display: ""
+        }
     }
 } );
