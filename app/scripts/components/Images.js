c32fa579e3b5235b1a36030fb8805451b3069b31
Nick The Sick <nperez0111@gmail.com>
2016-06-02 16:37:56 -0400

    Add Images displayer

---
+++ app/scripts/components/Images.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var Ractive = require( 'ractive/ractive.runtime' );
+
+var Image = Ractive.extend( {
+    template: require( './images.ract' ).template,
+    isolated: true,
+    computed: {
+        url: {
+            get: function () {
+                return this.get( "ctx" ).link;
+            }
+        },
+        width: {
+            get: function () {
+                return this.get( "ctx" ).width;
+            }
+        },
+        height: {
+            get: function () {
+                return this.get( "ctx" ).height;
+            }
+        },
+        ref: {
+            get: function () {
+                return this.get( "ctx" ).contextLink;
+            }
+        },
+        dispLink: {
+            get: function () {
+                return this.get( "titles" ).displayLink;
+            }
+        },
+        alt: {
+            get: function () {
+                return this.get( "titles" ).htmlTitle;
+            }
+        }
+    }
+} );
+
+module.exports = Image;
