5dbd29333d4f14b151013617bd49a24018fcf750
Nick The Sick <nperez0111@gmail.com>
2016-06-02 16:39:50 -0400

    Remove leftover components

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,6 @@
 'use strict';
 window.$ = require( 'jquery' );
 window.jQuery = window.$;
-require( './components/Modal.js' );
 var Main = require( './views/Main.js' );
 window.Router = require( './views/Router.js' );
 Router.route( {
